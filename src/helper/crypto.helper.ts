49e31d26a36873ffb25229bcd901a225819fbfaf
Billy He <hello@billyhe.com>
2022-12-30 14:01:32 -0800

    Update crypto helper to not use subtle crypto

--- src/helper/crypto.helper.ts
+++ src/helper/crypto.helper.ts
@@ -1,10 +1,7 @@
 import crypto from "crypto";
 
 export const sha1Digest = async (input: string): Promise<string> => {
-  const encoded = new TextEncoder().encode(input);
-  const digest = await crypto.subtle.digest("SHA-1", encoded);
-
-  return Array.from(new Uint8Array(digest))
-    .map((byte) => byte.toString(16))
-    .join("");
+  const sha1Hash = crypto.createHash("sha1");
+  sha1Hash.update(input);
+  return sha1Hash.digest("hex");
 };
