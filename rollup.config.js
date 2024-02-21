379fc2379602bedca0ec5fd0b547eabc3a4aecbf
Billie He <hello@billyhe.com>
2024-02-20 16:31:59 -0800

    Remove unused scripts from package.json

--- rollup.config.js
+++ rollup.config.js
@@ -17,6 +17,7 @@ export default {
     commonjs(),
     json(),
     typescript({
+      tsconfig: "./tsconfig.json",
       outputToFilesystem: true,
       compilerOptions: {
         declaration: false,
